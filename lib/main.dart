3836aac5a7a73c173478ca9820c0e64f1ba01d76
JanEuster <gh@jan.eusterschulte.com>
2023-01-10 15:41:43 +0100

    home nav layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,136 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import "dart:developer";
 
 void main() => runApp(new MyWidgetsApp());
 
 class MyWidgetsApp extends StatelessWidget {
+  final String title = "geo_steps";
 
-  Route generate(RouteSettings settings){
+  Route generate(RouteSettings settings) {
     Route page;
-    switch(settings.name){
+    switch (settings.name) {
       case "/":
-        page =  new PageRouteBuilder(
-            pageBuilder: (BuildContext context,Animation<double> animation,
-                Animation<double> secondaryAnimation){
-              return new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text("Home Page",textDirection: TextDirection.ltr,),
-                  const Padding(padding: const EdgeInsets.all(10.0)),
-                  new GestureDetector(
-                    onTap: () => Navigator.of(context).pushNamed("/first"),
-                    child: new Container(
-                      padding: const EdgeInsets.all(10.0),
-                      color:Colors.blue,
-                      child: const Text("Go to First Page"),
-                    ),
-                  ),
-                  const Padding(padding: const EdgeInsets.all(10.0)),
-                  new GestureDetector(
-                    onTap: () => Navigator.of(context).pushNamed("/abcd"),
-                    child: new Container(
-                      padding: const EdgeInsets.all(10.0),
-                      color:Colors.blue,
-                      child: const Text("Unkown Route"),
-                    ),
-                  )
-                ],
-              );
-            },
-            transitionsBuilder: (_, Animation<double> animation, Animation<double> second, Widget child) {
-              return new FadeTransition(
-                opacity: animation,
-                child: new FadeTransition(
-                  opacity: new Tween<double>(begin: 1.0, end: 0.0).animate(second),
-                  child: child,
-                ),
-              );
-            }
-        );
+        page = PageRouteBuilder(pageBuilder: (BuildContext context,
+            Animation<double> animation, Animation<double> secondaryAnimation) {
+          EdgeInsets insets = MediaQuery.of(context).viewInsets;
+          EdgeInsets padding = MediaQuery.of(context).viewPadding;
+          log("device insets || insets: $insets, padding: $padding");
+          return Container(
+              color: const Color.fromRGBO(255, 255, 255, 1.0),
+              child: Column(children: [
+                Padding(padding: EdgeInsets.only(top: padding.top)),
+                MyHomePage(title: title),
+                Padding(padding: EdgeInsets.only(top: padding.bottom)),
+              ]));
+        }, transitionsBuilder: (_, Animation<double> animation,
+            Animation<double> second, Widget child) {
+          return FadeTransition(
+            opacity: animation,
+            child: FadeTransition(
+              opacity: Tween<double>(begin: 1.0, end: 0.0).animate(second),
+              child: child,
+            ),
+          );
+        });
         break;
-      case "/first":
-        page =  new PageRouteBuilder(
-            pageBuilder: (BuildContext context,Animation<double> animation,
-                Animation<double> secondaryAnimation){
-              return new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text("First Page",textDirection: TextDirection.ltr,),
-                    const Padding(padding: const EdgeInsets.all(10.0)),
-                    new GestureDetector(
-                      onTap: () => Navigator.of(context).pop(),
-                      child: new Container(
-                        padding: const EdgeInsets.all(10.0),
-                        color:Colors.blue,
-                        child: const Text("Back"),
-                      ),
-                    )
-                  ]
-              );
-            },
-            transitionsBuilder: (_, Animation<double> animation, Animation<double> second, Widget child) {
-              return new FadeTransition(
-                opacity: animation,
-                child: new FadeTransition(
-                  opacity: new Tween<double>(begin: 1.0, end: 0.0).animate(second),
-                  child: child,
-                ),
-              );
-            }
-        );
-        break;
-      default: {
-        page = unKnownRoute(settings);
-      }
+      default:
+        {
+          page = unKnownRoute(settings);
+        }
     }
     return page;
   }
 
-  Route unKnownRoute(RouteSettings settings){
-    return new PageRouteBuilder(
-        pageBuilder: (BuildContext context,Animation<double> animation,
-            Animation<double> secondaryAnimation){
-          return new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text("First Page",textDirection: TextDirection.ltr,),
-                const Padding(padding: const EdgeInsets.all(10.0)),
-                new GestureDetector(
-                  onTap: () => Navigator.of(context).pop(),
-                  child: new Container(
-                    padding: const EdgeInsets.all(10.0),
-                    color:Colors.blue,
-                    child: const Text("Back"),
-                  ),
-                )
-              ]
-          );
-        }
-    );
+  Route unKnownRoute(RouteSettings settings) {
+    return PageRouteBuilder(pageBuilder: (BuildContext context,
+        Animation<double> animation, Animation<double> secondaryAnimation) {
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Error!",
+              textDirection: TextDirection.ltr,
+            ),
+            const Padding(padding: EdgeInsets.all(10.0)),
+            GestureDetector(
+              onTap: () => Navigator.of(context).pop(),
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                color: const Color.fromRGBO(0, 0, 255, 1.0),
+                child: const Text("Return to Home Page"),
+              ),
+            )
+          ]);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new WidgetsApp(
-        onGenerateRoute: generate,
-        onUnknownRoute: unKnownRoute,
-        textStyle: const TextStyle(),
-        initialRoute: "/",
-        color: Colors.red
+    return WidgetsApp(
+      onGenerateRoute: generate,
+      onUnknownRoute: unKnownRoute,
+      textStyle: const TextStyle(),
+      initialRoute: "/",
+      color: const Color.fromRGBO(255, 0, 0, 1.0),
+      title: title,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key, this.title = ""});
+
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+            color: Colors.white,
+            height: 45,
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        GestureDetector(onTap: () {}, child: Icon(Icons.menu)),
+                        Container(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            title,
+                            textScaleFactor: 1.2,
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(onTap: () {}, child: Icon(Icons.settings)),
+                ])),
+        Container(height: 1, color: Colors.black),
+        const Padding(padding: EdgeInsets.all(10.0)),
+        GestureDetector(
+          onTap: () => Navigator.of(context).pushNamed("/first"),
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            color: const Color.fromRGBO(0, 0, 255, 1.0),
+            child: const Text("Go to First Page"),
+          ),
+        ),
+        const Padding(padding: EdgeInsets.all(10.0)),
+        GestureDetector(
+          onTap: () => Navigator.of(context).pushNamed("/abcd"),
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            color: const Color.fromRGBO(0, 0, 255, 1.0),
+            child: const Text("Unkown Route"),
+          ),
+        )
+      ],
     );
   }
 }
