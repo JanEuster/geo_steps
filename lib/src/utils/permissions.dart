ed0106279f9fd2efa65e923796e0c31cddb0223d
JanEuster <gh@jan.eusterschulte.com>
2023-01-18 21:49:12 +0100

    only request manage_external_storage permission on android 11 and up

--- lib/src/utils/permissions.dart
+++ lib/src/utils/permissions.dart
@@ -1,20 +1,19 @@
+import 'dart:io';
+
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 import "dart:developer";
 
 Future<void> requestLocationAccess() async {
   // you need to request normal location access first, before being able to request locationAlways
   while (true) {
-    if ((await Permission.location
-        .request())
-        .isGranted) {
+    if ((await Permission.location.request()).isGranted) {
       break;
     }
   }
   while (true) {
-    if ((await Permission.locationAlways
-        .request())
-        .isGranted) {
+    if ((await Permission.locationAlways.request()).isGranted) {
       break;
     }
   }
@@ -22,32 +21,33 @@ Future<void> requestLocationAccess() async {
 
 Future<void> requestStorageAccess() async {
   while (true) {
-    if ((await Permission.storage
-        .request())
-        .isGranted) {
+    if ((await Permission.storage.request()).isGranted) {
       break;
     }
   }
+
   // on android 11+ access to all files is necessary for creating files in emulated storage folders
-  while (true) {
-    if ((await Permission.manageExternalStorage
-        .request())
-        .isGranted) {
-      break;
+  if (Platform.isAndroid) {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    int release = int.tryParse(androidInfo.version.release)!;
+    if (release >= 11) {
+      while (true) {
+        if ((await Permission.manageExternalStorage.request()).isGranted) {
+          break;
+        }
+      }
     }
   }
 }
 
 Future<void> requestNotificationAccess() async {
   while (true) {
-    if ((await Permission.notification
-        .request())
-        .isGranted) {
+    if ((await Permission.notification.request()).isGranted) {
       break;
     }
   }
 }
-
 
 Future<void> requestAllNecessaryPermissions() async {
   log("request all necessary permissions");
